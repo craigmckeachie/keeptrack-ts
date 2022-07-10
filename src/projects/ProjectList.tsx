8460130d4631823ecca8f959ea2c0925a56a9fd5
Craig McKeachie <craigdmckeachie@gmail.com>
2022-07-09 20:04:28 -0400

    lab07

--- src/projects/ProjectList.tsx
+++ src/projects/ProjectList.tsx
@@ -6,7 +6,24 @@ interface ProjectListProps {
 }
 
 function ProjectList({ projects }: ProjectListProps) {
-  return <pre>{JSON.stringify(projects, null, ' ')}</pre>;
+  return (
+    <div className="row">
+      {projects.map((project) => (
+        <div key={project.id} className="cols-sm">
+          <div className="card">
+            <img src={project.imageUrl} alt={project.name} />
+            <section className="section dark">
+              <h5 className="strong">
+                <strong>{project.name}</strong>
+              </h5>
+              <p>{project.description}</p>
+              <p>Budget : {project.budget.toLocaleString()}</p>
+            </section>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default ProjectList;
