c35ce899b71414524cf04182ea9c88f5af1c0439
Craig McKeachie <craigdmckeachie@gmail.com>
2022-07-09 20:04:25 -0400

    lab05

--- src/projects/ProjectsPage.tsx
+++ src/projects/ProjectsPage.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import { MOCK_PROJECTS } from './MockProjects';
 
 function ProjectsPage() {
-  return <h1>Projects</h1>;
+  return (
+    <>
+      <h1>Projects</h1>
+      <pre>{JSON.stringify(MOCK_PROJECTS, null, ' ')}</pre>
+    </>
+  );
 }
 
 export default ProjectsPage;
