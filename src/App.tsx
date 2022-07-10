d24de1275a68bd892fbfb84e6530270c13e0c4f4
Craig McKeachie <craigdmckeachie@gmail.com>
2022-07-09 20:08:04 -0400

    lab20

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,38 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  NavLink,
+} from 'react-router-dom';
 
 import './App.css';
+import HomePage from './home/HomePage';
 import ProjectsPage from './projects/ProjectsPage';
 
 function App() {
-  return <ProjectsPage />;
+  return (
+    <Router>
+      <header className="sticky">
+        <span className="logo">
+          <img src="/assets/logo-3.svg" alt="logo" width="49" height="99" />
+        </span>
+        <NavLink to="/" className="button rounded">
+          <span className="icon-home"></span>
+          Home
+        </NavLink>
+        <NavLink to="/projects/" className="button rounded">
+          Projects
+        </NavLink>
+      </header>
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/projects" element={<ProjectsPage />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
---
+++ src/home/HomePage.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function HomePage() {
+  return <h2>Home</h2>;
+}
+
+export default HomePage;
