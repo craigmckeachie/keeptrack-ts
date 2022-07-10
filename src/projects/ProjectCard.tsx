805b9b54d374a947d8cc6a8224ad690280f1ffa5
Craig McKeachie <craigdmckeachie@gmail.com>
2022-07-09 20:04:30 -0400

    lab09

--- src/projects/ProjectCard.tsx
+++ src/projects/ProjectCard.tsx
@@ -12,6 +12,10 @@ interface ProjectCardProps {
 function ProjectCard(props: ProjectCardProps) {
   const { project } = props;
 
+  const handleEditClick = (projectBeingEdited: Project) => {
+    console.log(projectBeingEdited);
+  };
+
   return (
     <div className="card">
       <img src={project.imageUrl} alt={project.name} />
@@ -21,6 +25,15 @@ function ProjectCard(props: ProjectCardProps) {
         </h5>
         <p>{formatDescription(project.description)}</p>
         <p>Budget : {project.budget.toLocaleString()}</p>
+        <button
+          className=" bordered"
+          onClick={() => {
+            handleEditClick(project);
+          }}
+        >
+          <span className="icon-edit "></span>
+          Edit
+        </button>
       </section>
     </div>
   );
