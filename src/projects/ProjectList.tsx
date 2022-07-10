aa41adf53ea07c0cf23b33585b3704520efde140
Craig McKeachie <craigdmckeachie@gmail.com>
2022-07-09 20:04:32 -0400

    lab12

--- src/projects/ProjectList.tsx
+++ src/projects/ProjectList.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Project } from './Project';
 import ProjectCard from './ProjectCard';
 import ProjectForm from './ProjectForm';
@@ -8,16 +8,20 @@ interface ProjectListProps {
 }
 
 function ProjectList({ projects }: ProjectListProps) {
+  const [projectBeingEdited, setProjectBeingEdited] = useState({});
   const handleEdit = (project: Project) => {
-    console.log(project);
+    setProjectBeingEdited(project);
   };
 
   return (
     <div className="row">
       {projects.map((project) => (
         <div key={project.id} className="cols-sm">
-          <ProjectCard project={project} onEdit={handleEdit} />
-          <ProjectForm />
+          {project === projectBeingEdited ? (
+            <ProjectForm />
+          ) : (
+            <ProjectCard project={project} onEdit={handleEdit} />
+          )}
         </div>
       ))}
     </div>
