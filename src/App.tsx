5426e56fd8cc65970312d130a07e76f71fde365a
Craig McKeachie <craigdmckeachie@gmail.com>
2022-07-09 20:04:24 -0400

    lab04

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
 import React from 'react';
 
 import './App.css';
+import ProjectsPage from './projects/ProjectsPage';
 
 function App() {
-  return (
-    <blockquote cite="Benjamin Franklin">
-      Tell me and I forget, teach me and I may remember, involve me and I learn.
-    </blockquote>
-  );
+  return <ProjectsPage />;
 }
 
 export default App;
