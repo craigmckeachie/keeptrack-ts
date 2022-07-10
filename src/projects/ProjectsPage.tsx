cbce5b6decc3ea3f7d25b762d92c80397d9d3b8c
Craig McKeachie <craigdmckeachie@gmail.com>
2022-07-09 20:07:56 -0400

    lab18

--- src/projects/ProjectsPage.tsx
+++ src/projects/ProjectsPage.tsx
@@ -1,10 +1,57 @@
-import React, { useState } from 'react';
-import { MOCK_PROJECTS } from './MockProjects';
+import React, { useEffect, useState } from 'react';
 import { Project } from './Project';
+import { projectAPI } from './projectAPI';
 import ProjectList from './ProjectList';
 
 function ProjectsPage() {
-  const [projects, setProjects] = useState(MOCK_PROJECTS);
+  const [projects, setProjects] = useState<Project[]>([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<string | undefined>(undefined);
+  const [currentPage, setCurrentPage] = useState(1);
+
+  // Approach 1: using promise then
+  // useEffect(() => {
+  //   setLoading(true);
+  //   projectAPI
+  //     .get(currentPage)
+  //     .then((data) => {
+  //       setLoading(false);
+  //       if (currentPage === 1) {
+  //         setProjects(data);
+  //       } else {
+  //         setProjects((projects) => [...projects, ...data]);
+  //       }
+  //     })
+  //     .catch((e) => {
+  //       setLoading(false);
+  //       if (e instanceof Error) {
+  //         setError(e.message);
+  //       }
+  //     });
+  // }, [currentPage]);
+
+  // Approach 2: using async/await
+  useEffect(() => {
+    async function loadProjects() {
+      setLoading(true);
+      try {
+        const data = await projectAPI.get(currentPage);
+        if (currentPage === 1) {
+          setProjects(data);
+        } else {
+          setProjects((projects) => [...projects, ...data]);
+        }
+      } catch (e) {
+        if (e instanceof Error) {
+          setError(e.message);
+        }
+      } finally {
+        setLoading(false);
+      }
+    }
+    loadProjects();
+  }, [currentPage]);
+
   const saveProject = (project: Project) => {
     let updatedProjects = projects.map((p: Project) => {
       return p.id === project.id ? project : p;
@@ -12,10 +59,47 @@ function ProjectsPage() {
     setProjects(updatedProjects);
   };
 
+  const handleMoreClick = () => {
+    setCurrentPage((currentPage) => currentPage + 1);
+  };
+
   return (
     <>
       <h1>Projects</h1>
+
+      {error && (
+        <div className="row">
+          <div className="card large error">
+            <section>
+              <p>
+                <span className="icon-alert inverse "></span>
+                {error}
+              </p>
+            </section>
+          </div>
+        </div>
+      )}
+
       <ProjectList projects={projects} onSave={saveProject} />
+
+      {!loading && !error && (
+        <div className="row">
+          <div className="col-sm-12">
+            <div className="button-group fluid">
+              <button className="button default" onClick={handleMoreClick}>
+                More...
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
+
+      {loading && (
+        <div className="center-page">
+          <span className="spinner primary"></span>
+          <p>Loading...</p>
+        </div>
+      )}
     </>
   );
 }
---
+++ src/projects/projectAPI.ts
@@ -0,0 +1,67 @@
+import { Project } from './Project';
+const baseUrl = 'http://localhost:4000';
+const url = `${baseUrl}/projects`;
+
+function translateStatusToErrorMessage(status: number) {
+  switch (status) {
+    case 401:
+      return 'Please login again.';
+    case 403:
+      return 'You do not have permission to view the project(s).';
+    default:
+      return 'There was an error retrieving the project(s). Please try again.';
+  }
+}
+
+function checkStatus(response: any) {
+  if (response.ok) {
+    return response;
+  } else {
+    const httpErrorInfo = {
+      status: response.status,
+      statusText: response.statusText,
+      url: response.url,
+    };
+    console.log(`log server http error: ${JSON.stringify(httpErrorInfo)}`);
+
+    let errorMessage = translateStatusToErrorMessage(httpErrorInfo.status);
+    throw new Error(errorMessage);
+  }
+}
+
+function parseJSON(response: Response) {
+  return response.json();
+}
+
+// eslint-disable-next-line
+function delay(ms: number) {
+  return function (x: any): Promise<any> {
+    return new Promise((resolve) => setTimeout(() => resolve(x), ms));
+  };
+}
+
+function convertToProjectModels(data: any[]): Project[] {
+  let projects: Project[] = data.map(convertToProjectModel);
+  return projects;
+}
+
+function convertToProjectModel(item: any): Project {
+  return new Project(item);
+}
+
+const projectAPI = {
+  get(page = 1, limit = 20) {
+    return fetch(`${url}?_page=${page}&_limit=${limit}&_sort=name`)
+      .then(checkStatus)
+      .then(parseJSON)
+      .then(convertToProjectModels)
+      .catch((error: TypeError) => {
+        console.log('log client error ' + error);
+        throw new Error(
+          'There was an error retrieving the projects. Please try again.'
+        );
+      });
+  },
+};
+
+export { projectAPI };
