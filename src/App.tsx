4d5b642aefa1fc955bc13f73999d20a8bff12203
Craig McKeachie <craigdmckeachie@gmail.com>
2022-07-09 20:04:23 -0400

    lab03

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React!!!
-        </a>
-      </header>
-    </div>
+    <blockquote cite="Benjamin Franklin">
+      Tell me and I forget, teach me and I may remember, involve me and I learn.
+    </blockquote>
   );
 }
 
