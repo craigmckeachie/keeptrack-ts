747ae010eff4b279a8d7bc584ca9ff87ef665725
Craig McKeachie <craigdmckeachie@gmail.com>
2022-07-09 20:04:30 -0400

    lab10

---
+++ src/projects/ProjectForm.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+function ProjectForm() {
+  return (
+    <form className="input-group vertical">
+      <label htmlFor="name">Project Name</label>
+      <input type="text" name="name" placeholder="enter name" />
+      <label htmlFor="description">Project Description</label>
+      <textarea name="description" placeholder="enter description" />
+      <label htmlFor="budget">Project Budget</label>
+      <input type="number" name="budget" placeholder="enter budget" />
+      <label htmlFor="isActive">Active?</label>
+      <input type="checkbox" name="isActive" />
+      <div className="input-group">
+        <button className="primary bordered medium">Save</button>
+        <span />
+        <button type="button" className="bordered medium">
+          cancel
+        </button>
+      </div>
+    </form>
+  );
+}
+
+export default ProjectForm;
--- src/projects/ProjectList.tsx
+++ src/projects/ProjectList.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Project } from './Project';
 import ProjectCard from './ProjectCard';
+import ProjectForm from './ProjectForm';
 
 interface ProjectListProps {
   projects: Project[];
@@ -12,6 +13,7 @@ function ProjectList({ projects }: ProjectListProps) {
       {projects.map((project) => (
         <div key={project.id} className="cols-sm">
           <ProjectCard project={project} />
+          <ProjectForm />
         </div>
       ))}
     </div>
