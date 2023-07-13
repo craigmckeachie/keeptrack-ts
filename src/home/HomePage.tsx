24fc4742c2e6fadea73ea3088ce4bb7e91c45d00
Craig McKeachie <craigdmckeachie@gmail.com>
2023-07-12 21:31:05 -0400

    alert working

--- src/home/HomePage.tsx
+++ src/home/HomePage.tsx
@@ -1,7 +1,32 @@
-import React from 'react';
+import { Alert, AlertDescription, AlertTitle } from '../components/ui/alert';
+import { Terminal } from 'lucide-react';
+
+export function AlertDemo() {
+  return (
+    <Alert>
+      x
+      <Terminal className="h-4 w-4" />
+      <AlertTitle>Heads up!</AlertTitle>
+      <AlertDescription>
+        You can add components to your app using the cli.
+      </AlertDescription>
+    </Alert>
+  );
+}
 
 function HomePage() {
-  return <h2>Home</h2>;
+  return (
+    <>
+      <h2 className="text-2xl">Home</h2>
+      <Alert>
+        <Terminal className="h-4 w-4" />
+        <AlertTitle>Heads up!</AlertTitle>
+        <AlertDescription>
+          You can add components and dependencies to your app using the cli.
+        </AlertDescription>
+      </Alert>
+    </>
+  );
 }
 
 export default HomePage;
