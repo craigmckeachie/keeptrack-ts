78b4a2126b8435b0ea96a0800e4d0c92fd6ca901
Craig McKeachie <craigdmckeachie@gmail.com>
2023-07-19 21:51:07 -0400

    home page

--- src/home/HomePage.tsx
+++ src/home/HomePage.tsx
@@ -4,7 +4,7 @@ function HomePage() {
   return (
     <>
       <h2 className="my-8 text-2xl font-bold">Home</h2>
-      <p>
+      {/* <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut non,
         quisquam placeat fuga officiis quia, voluptatem earum temporibus eaque
         similique commodi libero doloremque! Vel asperiores culpa consequuntur
@@ -12,7 +12,27 @@ function HomePage() {
         adipisicing elit. Possimus, ratione. Repellendus maxime sit ducimus
         temporibus inventore consequuntur delectus sequi, dignissimos et modi
         beatae consectetur sed ullam aut explicabo culpa id.
-      </p>
+      </p> */}
+      <div
+        className="hero min-h-screen"
+        style={{
+          backgroundImage:
+            'url(https://api.army.mil/e2/c/images/2023/06/30/18222f2d/max1200.jpg)',
+        }}
+      >
+        <div className="hero-overlay bg-opacity-60"></div>
+        <div className="hero-content text-center text-neutral-content">
+          <div className="max-w-md">
+            <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
+            <p className="mb-5">
+              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
+              et a id nisi.
+            </p>
+            <button className="btn btn-primary">Get Started</button>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
